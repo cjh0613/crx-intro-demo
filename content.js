e2325228317e5c65d38c0544c4e05ab6935d0785
cjh0613 <cjhbruce@foxmail.com>
2019-05-25 21:08:17 +0800

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,12 @@
+(function() {
+    'use strict';
 
+    // Your code here...
+    //$("#js_payChapter").hide()
+    //$("#js_payChapter").attr("style","display: none;");
+    var i=1;
+    while(i%10000==0){$("#js_payChapter").attr("style","display: none;");i++;}
+})();*/
+var id = setInterval(function(){
+$("#js_payChapter").attr("style","display: none;");
+}, 200)
